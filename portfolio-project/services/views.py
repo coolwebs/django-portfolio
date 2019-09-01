827f80a023cffae9ccbe8bd15c045e236f8c9eec
Ryan Archer <ryan@coolwebs.com.au>
2019-09-01 23:47:06 +0700

    setup postgre db and django model for services

--- portfolio-project/services/views.py
+++ portfolio-project/services/views.py
@@ -1,13 +1,11 @@
 # This is the default way the views file is written when starting new app via command line
 from django.shortcuts import render #can use this or the http method (but this is better I think)
+from .models import Service
 
 # This is what the tutorial docs tell me to do (https://docs.djangoproject.com/en/2.2/intro/tutorial01/)
 # from django.http import HttpResponse
 
-# Create your views here. (This was from lynda.com tutorial)
-# def nick(request):
-#     return render(request, 'services/home.html')
-
+# Create your views here
 def index(request):
-    # return HttpResponse("Hello, world. You're at the services index.")
-    return render(request, 'services/home.html')
+    services = Service.objects
+    return render(request, 'services/home.html', {'services': services})
