6672222dbb7cfa2a45ef9911b629412047459643
Ryan Archer <ryan@coolwebs.com.au>
2019-09-01 12:42:57 +0700

    update how the views are rendered (using shortcut/render instead of http method)

--- portfolio-project/services/views.py
+++ portfolio-project/services/views.py
@@ -1,12 +1,13 @@
 # This is the default way the views file is written when starting new app via command line
-# from django.shortcuts import render
+from django.shortcuts import render #can use this or the http method (but this is better I think)
 
 # This is what the tutorial docs tell me to do (https://docs.djangoproject.com/en/2.2/intro/tutorial01/)
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here. (This was from lynda.com tutorial)
 # def nick(request):
 #     return render(request, 'services/home.html')
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the services index.")
+    # return HttpResponse("Hello, world. You're at the services index.")
+    return render(request, 'services/home.html')
